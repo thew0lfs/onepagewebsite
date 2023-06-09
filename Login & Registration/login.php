d5890ac67b985e2d15bf5ee2440c8cb23ea1aa5b
amirrezafalian <133581630+amirrezafalian@users.noreply.github.com>
2023-06-09 11:11:58 -0700

    Update login.php

--- Login & Registration/login.php
+++ Login & Registration/login.php
@@ -1,22 +1,29 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  $user = $_POST['username'];
+  $user = $_POST['name'];
   $pass = $_POST['password'];
 
   $con = mysqli_connect('localhost' , 'root' , '');
 
   mysqli_select_db($con , 'users');
   
-  $result = mysqli_query($con , "SELECT * FROM 'users'  WHERE 'user' = '$user'   ");
+  $result = mysqli_query($con , "SELECT * FROM  users  WHERE user = '$user'   ");
 
-  while($row == mysqli_fetch_array($result)){
+  if ($row == mysqli_fetch_array($result)){
     if ($pass == $row['password']){
-      echo "ok"
+      header("location: index.html ");
     }
     else {
-      echo "NO?"
+      echo "NO?";
     }
 
 }
 ?>
+
+
+
+
+
+
+
