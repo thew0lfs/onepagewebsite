b4c5d04766e6cecb41f326aa342597342b8009ac
amirrezafalian <133581630+amirrezafalian@users.noreply.github.com>
2023-06-09 10:00:51 -0700

    Update register.php

--- Login & Registration/register.php
+++ Login & Registration/register.php
@@ -1,16 +1,3 @@
-
-Fatal error: Uncaught TypeError: mysqli_select_db(): Argument #1 ($mysql) must be of type mysqli, string given in C:\xampp\htdocs\onepagewebsite-main\Login & Registration\register.php:9 Stack trace: #0 C:\xampp\htdocs\onepagewebsite-main\Login & Registration\register.php(9): mysqli_select_db('$mysql', 'users') #1 {main} thrown in C:\xampp\htdocs\onepagewebsite-main\Login & Registration\register.php on line 9
-
-
-
-
-
-
-
-
-
-
-
 <?php
 
 $name = $_POST['name'];
@@ -19,8 +6,7 @@ $pass = $_POST['password'];
 
 $mysql = mysqli_connect('localhost' , 'root' , '');
 
-mysqli_select_db('$mysql' , 'users');
+mysqli_select_db($mysql , 'users');
 
-mysqli_query('$mysql' , "INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES ('','$name','$email','$pass')");
+mysqli_query( $mysql , "INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES ('','$name','$email','$pass')");
 ?>
-
