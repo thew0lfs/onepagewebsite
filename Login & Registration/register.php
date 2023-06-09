cce31e9791d8f710028ff7d9c81746bfd4129380
thew0lfs <47688737+thew0lfs@users.noreply.github.com>
2023-06-09 21:58:26 +0330

    Update register.php

--- Login & Registration/register.php
+++ Login & Registration/register.php
@@ -9,4 +9,7 @@ $mysql = mysqli_connect('localhost' , 'root' , '');
 mysqli_select_db($mysql , 'users');
 
 mysqli_query( $mysql , "INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES ('','$name','$email','$pass')");
+
+header("Location: registered.html");
+exit();
 ?>
