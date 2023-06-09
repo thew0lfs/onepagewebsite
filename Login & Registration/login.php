0c8fee09bd157c25f26ef3b450799ec1e2683cfe
amirrezafalian <133581630+amirrezafalian@users.noreply.github.com>
2023-06-09 19:00:34 +0330

    Update login.php

--- Login & Registration/login.php
+++ Login & Registration/login.php
@@ -1,19 +1,38 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Retrieve the submitted form data
+
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Perform authentication logic
-  // Here you can validate the username and password against your database or any other authentication mechanism
-  // For demonstration purposes, let's assume the username is "admin" and the password is "password"
-  if ($username === 'admin' && $password === 'password') {
-    // Authentication successful, redirect to a welcome page or perform further actions
-    header('Location: welcome.php');
-    exit;
-  } else {
-    // Authentication failed, display an error message or perform any other necessary actions
-    echo 'Invalid username or password';
-  }
+  $server = mysqli_connect('localhost' , 'root' , '');
+
+
+  mysqli_select_db('$sever' , 'users');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 ?>
