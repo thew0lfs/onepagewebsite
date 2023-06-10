656a67318b56ee14e80f26219cb126f4ddb00dc8
thew0lfs <47688737+thew0lfs@users.noreply.github.com>
2023-06-10 23:40:19 +0330

    Update PostDetail.php

--- post/PostDetail.php
+++ post/PostDetail.php
@@ -34,6 +34,52 @@
             line-height: 1.5;
             margin: 0;
         }
+
+        h2 {
+            font-size: 24px;
+            margin-top: 20px;
+        }
+
+        form {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            font-size: 16px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        ul li {
+            margin-bottom: 10px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
     </style>
 </head>
 
