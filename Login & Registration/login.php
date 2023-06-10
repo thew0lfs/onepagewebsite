ebdfc8d3d5072dce7c4e019145d3882f8d93724e
thew0lfs <47688737+thew0lfs@users.noreply.github.com>
2023-06-10 19:27:21 +0330

    Update login.php

--- Login & Registration/login.php
+++ Login & Registration/login.php
@@ -1,29 +1,21 @@
 <?php
 
-  
-  $user = $_POST['name'];
-  $pass = $_POST['password'];
 
-  $con = mysqli_connect('localhost' , 'root' , '');
+$user = $_POST['name'];
+$pass = $_POST['password'];
 
-  mysqli_select_db($con , 'users');
-  
-  $result = mysqli_query($con , "SELECT * FROM  `users`  WHERE  `name` = '$user'   ");
-  
-  while ($row = mysqli_fetch_array($result)){
-    if ($pass == $row['password']){
-      header("location: register.html");
-    }
-    else {
-      echo "NO?";
-    }
+$con = mysqli_connect('localhost', 'root', '');
+
+mysqli_select_db($con, 'users');
+
+$result = mysqli_query($con, "SELECT * FROM  `users`  WHERE  `name` = '$user'   ");
+
+while ($row = mysqli_fetch_array($result)) {
+  if ($pass == $row['password']) {
+    header("location: signed-in.html");
+  } else {
+    header("location: incorrect-data.html");
+  }
 
 }
 ?>
-
-
-
-
-
-
-
